a7875ab8d44d85d9b4357e312492846ff2ecb784
Thiago Rodrigues <trodrigues@fitec.org.br>
2018-10-24 20:40:41 -0300

    Cálculo de notas e centavos completo.

--- src/CalcularTroco.java
+++ src/CalcularTroco.java
@@ -13,24 +13,94 @@ import java.util.Scanner;
 
 public class CalcularTroco
 {
-    private int nota100 = 0;
-    private int nota50 = 0;
-    private int nota10 = 0;
-    private int nota5 = 0;
-    private int nota1 = 0;
-    private int moeda50 = 0;
-    private int moeda10 = 0;
-    private int moeda5 = 0;
-    private int moeda1 = 0;
+    private static int nota100;
+    private static int nota50;
+    private static int nota10;
+    private static int nota5;
+    private static int nota1;
+    private static int moeda50;
+    private static int moeda10;
+    private static int moeda5;
+    private static int moeda1;
 
     /**
-     * @param valor Valor total a ser recebido
+     * @param valorConta Valor total da conta
+     * @param valorPago Valor pago pelo cliente
      */
-    public static void calcularTroco(int valor)
+    public static String calcularTroco(double valorConta, double valorPago)
     {
+        if (valorPago < valorConta)
+        {
+            return "Valor Insuficiente";
+        }
+        else
+        {
+            int listaNota[] =
+            { 100, 50, 10, 5, 1 };
+            int listaCentavos[] =
+            { 50, 10, 5, 1 };
 
-        //for(int i = 0; i < )
-        // if(valor % 100 == 0)
+            String resultado;
+            double troco = valorPago - valorConta;
+            int i = 0, valor, centavos, numeroNotas;
+
+            // calculando quantidade de notas
+            valor = (int) troco;
+            while (valor != 0)
+            {
+                numeroNotas = valor / listaNota[i];
+                if (numeroNotas != 0)
+                {
+                    valor = valor % listaNota[i];
+
+                    if (listaNota[i] == 100) nota100 = numeroNotas;
+                    if (listaNota[i] == 50) nota50 = numeroNotas;
+                    if (listaNota[i] == 10) nota10 = numeroNotas;
+                    if (listaNota[i] == 5) nota5 = numeroNotas;
+                    if (listaNota[i] == 1) nota1 = numeroNotas;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            // definindo os centavos do troco (parte fracionária)
+            valor = (int) Math.round((troco - (int) troco) * 100);
+            i = 0;
+            while (valor != 0)
+            {
+                centavos = valor / listaCentavos[i]; // calculando a qtde de moedas
+                if (centavos != 0)
+                {
+                    valor = valor % listaCentavos[i]; // sobra
+
+                    if (listaCentavos[i] == 50) moeda50 = centavos;
+                    if (listaCentavos[i] == 10) moeda10 = centavos;
+                    if (listaCentavos[i] == 5) moeda5 = centavos;
+                    if (listaCentavos[i] == 1) moeda1 = centavos;
+                }
+                else
+                {
+                    i = i + 1; // próximo centavo
+                }
+            }
+        }
+        return "";
+    }
+
+    public static void exibirResultados()
+    {
+        if (nota100 > 0) System.out.println("Notas de 100: " + nota100);
+        if (nota50 > 0) System.out.println("Notas de 50: " + nota50);
+        if (nota10 > 0) System.out.println("Notas de 10: " + nota10);
+        if (nota5 > 0) System.out.println("Notas de 5: " + nota5);
+        if (nota1 > 0) System.out.println("Notas de 1: " + nota1);
+
+        if (moeda50 > 0) System.out.println("Moedas de 50: " + moeda50);
+        if (moeda10 > 0) System.out.println("Moedas de 10: " + moeda10);
+        if (moeda5 > 0) System.out.println("Moedas de 5: " + moeda5);
+        if (moeda1 > 0) System.out.println("Moedas de 1: " + moeda1);
     }
 
     /**
@@ -49,7 +119,11 @@ public class CalcularTroco
         System.out.println("O valor total pago " + valorPago);
 
         scanner.close();
-        System.out.println("O valor total pago " + valorPago);
+
+        calcularTroco(valorConta, valorPago);
+
+        System.out.println("\n\nTROCO\n");
+        exibirResultados();
     }
 
 }
